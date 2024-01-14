d80f801f5e0ca13efca1b3353fdaa92343daee3d
hardhus <ahmetcemiltoprakk@gmail.com>
2024-01-14 20:06:15 +0300

    Add btn command implementations, excluding eq btn

--- main.py
+++ main.py
@@ -20,33 +20,122 @@ class App(ctk.CTk):
         self.inputScreen.grid(row=0, column=0, sticky="new", padx=10, pady=50)
         self.numpadFrame.grid(row=1, column=0, sticky="nsew", padx=10, pady=10)
 
-    @classmethod
-    def test(cls):
-        print("test")
-
 class NumpadFrame(ctk.CTkFrame):
     def __init__(self, master: Any):
         super().__init__(master=master)
         self.createWidgets()
         self.placeWidgets()
 
+        self.index = 0
+
+    def btn_0_command(self):
+        self.master.inputScreen.insert(self.index, "0")
+        self.index += 1
+
+    def btn_1_command(self):
+        self.master.inputScreen.insert(self.index, "1")
+        self.index += 1
+
+    def btn_2_command(self):
+        self.master.inputScreen.insert(self.index, "2")
+        self.index += 1
+
+    def btn_3_command(self):
+        self.master.inputScreen.insert(self.index, "3")
+        self.index += 1
+
+    def btn_4_command(self):
+        self.master.inputScreen.insert(self.index, "4")
+        self.index += 1
+
+    def btn_5_command(self):
+        self.master.inputScreen.insert(self.index, "5")
+        self.index += 1
+
+    def btn_6_command(self):
+        self.master.inputScreen.insert(self.index, "6")
+        self.index += 1
+
+    def btn_7_command(self):
+        self.master.inputScreen.insert(self.index, "7")
+        self.index += 1
+
+    def btn_8_command(self):
+        self.master.inputScreen.insert(self.index, "8")
+        self.index += 1
+
+    def btn_9_command(self):
+        self.master.inputScreen.insert(self.index, "9")
+        self.index += 1
+
+    def btn_eq_command(self):
+        currentText = self.master.inputScreen.get()
+        result = self.calculate(currentText)
+
+    def btn_del_command(self):
+        self.master.inputScreen.delete(self.index - 1, ctk.END)
+        self.index -= 1
+
+    def btn_plus_command(self):
+        currentText = self.master.inputScreen.get()
+        if currentText and currentText[-1] in ["+", "-", "*", "/"]:
+            newText = currentText[:-1] + "+"
+        else:
+            newText = currentText + "+"
+        self.master.inputScreen.delete(0, ctk.END)
+        self.master.inputScreen.insert(0, newText)
+        self.index = len(newText)
+
+    def btn_minus_command(self):
+        currentText = self.master.inputScreen.get()
+        if currentText and currentText[-1] in ["+", "-", "*", "/"]:
+            newText = currentText[:-1] + "-"
+        else:
+            newText = currentText + "-"
+        self.master.inputScreen.delete(0, ctk.END)
+        self.master.inputScreen.insert(0, newText)
+        self.index = len(newText)
+
+    def btn_multiply_command(self):
+        currentText = self.master.inputScreen.get()
+        if currentText and currentText[-1] in ["+", "-", "*", "/"]:
+            newText = currentText[:-1] + "*"
+        else:
+            newText = currentText + "*"
+        self.master.inputScreen.delete(0, ctk.END)
+        self.master.inputScreen.insert(0, newText)
+        self.index = len(newText)
+
+    def btn_divide_command(self):
+        currentText = self.master.inputScreen.get()
+        if currentText and currentText[-1] in ["+", "-", "*", "/"]:
+            newText = currentText[:-1] + "/"
+        else:
+            newText = currentText + "/"
+        self.master.inputScreen.delete(0, ctk.END)
+        self.master.inputScreen.insert(0, newText)
+        self.index = len(newText)
+
+    def calculate(currentText):
+        pass
+
     def createWidgets(self):
-        self.btn_0 = ctk.CTkButton(       self, text="0",   width=50, height=50)
-        self.btn_1 = ctk.CTkButton(       self, text="1",   width=50, height=50)
-        self.btn_2 = ctk.CTkButton(       self, text="2",   width=50, height=50)
-        self.btn_3 = ctk.CTkButton(       self, text="3",   width=50, height=50)
-        self.btn_4 = ctk.CTkButton(       self, text="4",   width=50, height=50)
-        self.btn_5 = ctk.CTkButton(       self, text="5",   width=50, height=50)
-        self.btn_6 = ctk.CTkButton(       self, text="6",   width=50, height=50)
-        self.btn_7 = ctk.CTkButton(       self, text="7",   width=50, height=50)
-        self.btn_8 = ctk.CTkButton(       self, text="8",   width=50, height=50)
-        self.btn_9 = ctk.CTkButton(       self, text="9",   width=50, height=50)
-        self.btn_eq = ctk.CTkButton(      self, text="=",   width=50, height=50)
-        self.btn_del = ctk.CTkButton(     self, text="DEL", width=50, height=50)
-        self.btn_plus = ctk.CTkButton(    self, text="+",   width=50, height=50)
-        self.btn_minus = ctk.CTkButton(   self, text="-",   width=50, height=50)
-        self.btn_multiply = ctk.CTkButton(self, text="*",   width=50, height=50)
-        self.btn_divide = ctk.CTkButton(  self, text="/",   width=50, height=50)
+        self.btn_0 = ctk.CTkButton(       self, text="0",   width=60, height=60, command=self.btn_0_command)
+        self.btn_1 = ctk.CTkButton(       self, text="1",   width=60, height=60, command=self.btn_1_command)
+        self.btn_2 = ctk.CTkButton(       self, text="2",   width=60, height=60, command=self.btn_2_command)
+        self.btn_3 = ctk.CTkButton(       self, text="3",   width=60, height=60, command=self.btn_3_command)
+        self.btn_4 = ctk.CTkButton(       self, text="4",   width=60, height=60, command=self.btn_4_command)
+        self.btn_5 = ctk.CTkButton(       self, text="5",   width=60, height=60, command=self.btn_5_command)
+        self.btn_6 = ctk.CTkButton(       self, text="6",   width=60, height=60, command=self.btn_6_command)
+        self.btn_7 = ctk.CTkButton(       self, text="7",   width=60, height=60, command=self.btn_7_command)
+        self.btn_8 = ctk.CTkButton(       self, text="8",   width=60, height=60, command=self.btn_8_command)
+        self.btn_9 = ctk.CTkButton(       self, text="9",   width=60, height=60, command=self.btn_9_command)
+        self.btn_eq = ctk.CTkButton(      self, text="=",   width=60, height=60, command=self.btn_eq_command)
+        self.btn_del = ctk.CTkButton(     self, text="DEL", width=60, height=60, command=self.btn_del_command)
+        self.btn_plus = ctk.CTkButton(    self, text="+",   width=60, height=60, command=self.btn_plus_command)
+        self.btn_minus = ctk.CTkButton(   self, text="-",   width=60, height=60, command=self.btn_minus_command)
+        self.btn_multiply = ctk.CTkButton(self, text="*",   width=60, height=60, command=self.btn_multiply_command)
+        self.btn_divide = ctk.CTkButton(  self, text="/",   width=60, height=60, command=self.btn_divide_command)
 
     def placeWidgets(self):
         self.btn_0.grid(       row=3, column=0, padx=10, pady=10, sticky="nsew")
